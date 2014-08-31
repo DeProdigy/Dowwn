88afa4ccf126c6b17be986482f4f65c7de07426d
DeProdigy <alex.hint.ah@gmail.com>
2014-08-30 21:07:36 -0400

    Reset the project to the first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Dowwn::Application.routes.draw do
   root to: 'welcome#index'
   get "welcome/index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
